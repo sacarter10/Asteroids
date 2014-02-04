94890dd3d6b2fda25dcca8ae2ffb8e3fbc9f8a12
Sally Carter <sacarter10@gmail.com>
2014-02-03 22:11:47 -0800

    owww

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function () {
   var canvas = $("<canvas width='" + 1000 +
                  "' height='" + 800 + "'></canvas>");
-  $('body').append(canvas);
+  $('#main-content').append(canvas);
 
   new Asteroids.Game(1000, 800, canvas.get(0)).start();
 });
