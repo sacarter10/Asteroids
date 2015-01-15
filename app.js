7ac5892d0235c87fe0129134cf52fab873ff72a1
Sally Carter <sacarter10@gmail.com>
2015-01-14 17:13:43 -0800

    start out with canvas element on page

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 $(function () {
-  var canvas = $("<canvas width='" + 1000 +
-                 "' height='" + 800 + "'></canvas>");
-  $('#main-content').append(canvas);
+  var canvas = $(canvas);
 
   new Asteroids.Game(1000, 800, canvas.get(0)).start();
 });
